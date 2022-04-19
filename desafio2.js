8d05837cceaa2042eb2afad9e0b27c6a646fadb0
unknown <alexsandrapcampos03@gmail.com>
2022-04-18 23:56:02 -0300

    fiz logica do exercicio dois

---
+++ desafio2.js
@@ -0,0 +1,10 @@
+const numero1=10
+const numero2=20
+
+if(numero1 > numero2){
+    console.log(numero1)
+}else if(numero2>numero1){
+    console.log(numero2)
+}else{
+  console.log("eles sao iguais")
+}
