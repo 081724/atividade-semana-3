db8591d2c5439178262581ed16831f7a8120d7cf
unknown <alexsandrapcampos03@gmail.com>
2022-04-18 23:31:05 -0300

    fiz logica solicitada do exercicio1

---
+++ desafio1.js
@@ -0,0 +1,28 @@
+const numero = 10
+
+switch (numero) {
+    case 1:
+        console.log("segunda")
+        break;
+    case 2:
+        console.log("terca")
+        break;
+    case 3:
+        console.log("quarta")
+        break;
+    case 4:
+        console.log("quinta")
+        break;
+    case 5:
+        console.log("sexta")
+        break;
+    case 6:
+        console.log("sabado")
+        break;
+    case 7:
+        console.log("domingo")
+        break;
+
+    default:
+      console.log(  numero + " nao representa o dia da semana ")
+}
