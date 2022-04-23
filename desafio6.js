c78d415d9b34881a43019a6a13e50d8517a5b0da
unknown <alexsandrapcampos03@gmail.com>
2022-04-22 22:34:51 -0300

    concluir desafio6 com sucesso

---
+++ desafio6.js
@@ -0,0 +1,9 @@
+// crie um algoritmo que converte dias em horas, quando recebe um número de
+// dias.
+function diasEHoras(dias){
+    let horas=dias*24
+    console.log(horas)
+
+}
+diasEHoras(3)
+ diasEHoras(5)
